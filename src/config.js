bace4194b31b80946572ff00d10c345780bb0d0e
ueozarks <43754732+ueozarks@users.noreply.github.com>
2018-10-01 12:15:21 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import { projects } from 'json!../.firebaserc'
 
-export const apiKey = "AIzaSyBuSzNftbHVOOhxirAnQRqOSFIq1V9R09M"
+export const apiKey = "AIzaSyDimxs7HXMRrmg6ql2if9xxtgdWSAtgn0s"
 export const projectId = projects.default
